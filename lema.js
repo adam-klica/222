624d759de7d0c8af82790d53135dbbb3f15ade5a
Adam <adam.klica@ninjacloud.rs>
2022-08-31 00:20:10 +0200

    port

--- lema.js
+++ lema.js
@@ -58,7 +58,7 @@ const server = http.createServer(function(request, response) {
   }
 })
 
-const port = 3000
+const port = 80
 const host = 'https://tranquil-cove-88653.herokuapp.com'
 server.listen(port, host)
 console.log(`Listening at http://${host}:${port}`)
